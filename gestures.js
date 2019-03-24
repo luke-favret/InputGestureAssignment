4c2f7785ec0ade58db4ca0f00e3146af5ec92798
luke-favret <45835843+luke-favret@users.noreply.github.com>
2019-03-24 00:56:59 -0600

    Added instructions and extra credit

--- gestures.js
+++ gestures.js
@@ -1,5 +1,5 @@
 
-var isMouseDown = 0;
+var clickTotal = 0;
 
 var startX = 0;
 var startY = 0;
@@ -7,7 +7,7 @@ var startY = 0;
 $("#gestureArea").mousedown(function (event) {
                             startX = event.pageX;
                             startY = event.pageY;
-                            $("#gestureResult").val('startX'');
+                            document.getElementById('gestureResult').textContent = "Gesturing...";
                             });
 
 var xMov = 0;
@@ -16,14 +16,28 @@ var yMov = 0;
 $("#gestureArea").mouseup(function (event) {
                         xMov = event.pageX - startX;
                         yMov = event.pageY - startY;
-                          if( xMov > 0 && xMov > Abs(yMov)){
-    //Right Swipe
-}else if(yMov > 0 && yMov > Abs(xMov){
-         //Down Swipe
-         }else if(xMov < 0 && Abs(xMov) > Abs(yMov)){
+                        //document.getElementById('gestureResult').textContent = Math.abs(xMov);
+                          if( xMov > 0 && xMov > Math.abs(yMov)){
+                              document.getElementById('gestureResult').textContent = "Right Swipe"; //Right Swipe
+                              clickTotal = 0;
+}else if(yMov > 0 && yMov > Math.abs(xMov)) {
+     document.getElementById('gestureResult').textContent = "Down Swipe"; //Down Swipe
+    clickTotal = 0;
+         }else if(xMov < 0 && Math.abs(xMov) > Math.abs(yMov)){
+              document.getElementById('gestureResult').textContent = "Left Swipe";
+             clickTotal = 0;
     //Left swipe
-}else if(yMov < 0 && Abs(yMov) > Abs(xMov)){
+}else if(yMov < 0 && Math.abs(yMov) > Math.abs(xMov)){
+     document.getElementById('gestureResult').textContent = "Up Swipe";
+    clickTotal = 0;
     //Up Swipe
+}else if(yMov == 0 && xMov == 0){
+    clickTotal += 1;
+    if(clickTotal == 2){
+        document.getElementById('gestureResult').textContent = "Double Click";
+    }else if(clickTotal == 3){
+        document.getElementById('gestureResult').textContent = "Triple Click";
+    }
 }
                           
                         
